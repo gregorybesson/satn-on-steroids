37b7e21009851bd2e4f95988a3975505334c4fcc
Greg Besson <bessong@gmail.com>
2022-09-09 10:27:45 +0200

    fix

--- web/index.js
+++ web/index.js
@@ -170,7 +170,6 @@ export async function createServer(
   const modules = process.env.MODULES?.split(",") || [];
   for (const module of modules) {
     const mod = await import(module);
-    mod.shopifyInit();
     app.use('/app', mod.router);
     app.use(`/app/${module}/api/*`,verifyRequest(app, { billing: billingSettings}));
   };
